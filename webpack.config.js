9287be33ee677869984a2d0915e346dc182632cb
Doug Martin <doug@zoopdoop.com>
2018-12-06 14:54:34 -0500

    Update iframes to point to CODAP staging and sage master

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,9 @@ module.exports = (env, argv) => {
           tag: packageJSON.version
         },
         buildInfoString = `${buildInfo.date} ${buildInfo.tag}`,
-        cfmUrl = devMode ? "/cfm" : "https://cloud-file-manager.concord.org/branch/master",
-        codapUrl = devMode ? "/codap/static/dg/en/cert/index.html" : "/~dmartin/travis4/static/dg/en/cert/index.html",
-        sageUrl = devMode ? "/sage" : "https://sage.concord.org/branch/master";
+        cfmUrl = "/cfm",
+        codapUrl = devMode ? "/codap/static/dg/en/cert/index.html" : "/releases/staging/static/dg/en/cert/index.html",
+        sageUrl = devMode ? "/sage" : "/sage/branch/master";
 
   return {
     context: __dirname, // to automatically find tsconfig.json
