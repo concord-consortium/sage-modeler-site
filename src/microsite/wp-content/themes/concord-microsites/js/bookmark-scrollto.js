e70b941619566d9ab429c70066b79cd6c4d2a0df
Doug Martin <dmartin@concord.org>
2019-04-29 16:10:03 -0400

    Fixed bookmark scroll issue

--- src/microsite/wp-content/themes/concord-microsites/js/bookmark-scrollto.js
+++ src/microsite/wp-content/themes/concord-microsites/js/bookmark-scrollto.js
@@ -2,8 +2,12 @@
   $(document).ready(function() {
     $('.toc a').click(function(e) {
       e.preventDefault();
+      var targetOffset = 220;
+      if ($('.concord-header-contain').hasClass('fixed')) {
+        targetOffset = 100;
+      }
       var targetID = jQuery(this).attr('href');
-      var targetLoc = $(targetID).offset().top - 100;
+      var targetLoc = $(targetID).offset().top - targetOffset;
       $('html, body').animate({scrollTop: targetLoc}, 500);
     });
     $('a[href="#top"]').click(function(e) {
