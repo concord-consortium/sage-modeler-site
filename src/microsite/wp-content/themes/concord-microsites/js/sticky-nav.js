b8f3176d7b5aa24b0eaa2e1b19dcd5e3e6ebee51
Ethan McElroy <emcelroy@concord.org>
2020-10-19 16:17:19 -0400

    Updated microsite content

--- src/microsite/wp-content/themes/concord-microsites/js/sticky-nav.js
+++
@@ -1,9 +0,0 @@
-;(function($, undefined){
-  $(window).scroll(function() {
-    if ($(window).width() >= 951 && $(window).scrollTop() > 5) {
-      $('.concord-header-contain').addClass('fixed');
-    } else {
-      $('.concord-header-contain').removeClass('fixed');
-    }
-  });
-})(jQuery);
