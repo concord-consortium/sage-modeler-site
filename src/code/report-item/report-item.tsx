a808a135a122b9d98f8e7eb5842ad25122c38a8f
Doug Martin <dmartin@concord.org>
2022-10-17 10:41:27 -0400

    Upgraded report-item to use v2 of the report item api [#183557683]

--- src/code/report-item/report-item.tsx
+++ src/code/report-item/report-item.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
+import * as semver from "semver";
 import { IReportItemInitInteractive,
          addGetReportItemAnswerListener,
          sendReportItemAnswer,
-         getClient } from "@concord-consortium/lara-interactive-api";
+         getClient,
+         IReportItemAnswerItem} from "@concord-consortium/lara-interactive-api";
 import { MetricsLegendComponent, metricsReportItemHtml } from "./metrics";
 
 interface Props {
@@ -16,22 +18,38 @@ export const ReportItemComponent: React.FC<Props> = (props) => {
   const [userAnswers, setUserAnswers] = useState<Record<string, any>>({});
 
   useEffect(() => {
-    addGetReportItemAnswerListener(async (request) => {
-      const {type, platformUserId, interactiveState, authoredState} = request;
+    if (initMessage && initMessage.mode === "reportItem") {
+      const {interactiveItemId} = initMessage;
 
-      setUserAnswers(prev => ({...prev, [platformUserId]: interactiveState}));
+      addGetReportItemAnswerListener(async (request) => {
+        // TODO: update lara interactive api to change addGetReportItemAnswerListener to a generic with <IInteractiveState, IAuthoredState>
+        // and remove the `any` after request
+        const { platformUserId, interactiveState, authoredState, version } = request as any;
 
-      switch (type) {
-        case "html":
+        if (!version) {
+          // for hosts sending older, unversioned requests
+          console.error("Missing version in getReportItemAnswer request.");
+        } else if (semver.satisfies(version, "2.x")) {
           const html = await metricsReportItemHtml({interactiveState, platformUserId, interactiveItemId});
-          sendReportItemAnswer({type: "html", platformUserId, html});
-          break;
-      }
-    });
+          const items: IReportItemAnswerItem[] = [
+            {
+              type: "html",
+              html
+            }
+          ];
+          sendReportItemAnswer({version, platformUserId, items, itemsType: "fullAnswer"});
+        } else {
+          console.error("Unsupported version in getReportItemAnswer request:", version);
+        }
+      });
+      getClient().post("reportItemClientReady");
+    }
+  }, [initMessage]);
 
-    // tell the portal-report we are ready for messages
-    getClient().post("reportItemClientReady");
-  }, []);
+  // do not render anything if hidden
+  if (view === "hidden") {
+    return null;
+  }
 
   return (
     <div className={`reportItem ${view}`}>
